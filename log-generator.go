c3c5390d7180d755c369df8694bbf3c1eaeff968
someone <y34rz3r@proton.me>
2024-02-03 18:27:08 +0300

    log-generator.go

--- log-generator.go
+++ log-generator.go
@@ -11,17 +11,15 @@ func main() {
 		min := 1
 		max := 5
 		currentTime := time.Now().Format("2006.01.02 15:04:05\n")
-		randomMinute := rand.Intn(max-min) + min
+		randomSecond := rand.Intn(max-min) + min
 
-		f, _ := os.OpenFile("/var/log/nginx/test.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f, _ := os.OpenFile("/var/log/test.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		f.Write([]byte(currentTime))
 		f.Close()
 
-		time.Sleep(time.Duration(randomMinute) * time.Minute)
+		time.Sleep(time.Duration(randomSecond) * time.Second)
 	}
 }
 
-// Build:
-// GOOS=linux GOARCH=amd64 go build log-generator.go
-// Launch: 
-// nohup ./log-generator &
+// Launch hint:
+// sudo nohup ./log-generator &
